a644361fe5f8765ba33d8a5afdb968329b3596cc
Robin Winslow <robin@robinwinslow.co.uk>
2014-01-05 22:47:20 +0000

    Include south in app

--- msq/settings.py
+++ msq/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'questionnaire'
+    'questionnaire',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
