a9d7776aef6ce3360a28a83ae5bd43afd89e89ac
Robin Winslow <robin@robinwinslow.co.uk>
2014-01-05 22:42:27 +0000

    Party tests and models

--- questionnaire/models.py
+++ questionnaire/models.py
@@ -4,3 +4,10 @@ class Constituency(models.Model):
     """ Database model for constituencies """
 
     name = models.CharField(max_length=200)
+
+class Party(models.Model):
+    """ Database model for political parties """
+
+    name = models.CharField(max_length=200)
+    abbreviation = models.CharField(max_length=10)
+ 
--- questionnaire/tests.py
+++ questionnaire/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from questionnaire.models import Constituency
+from questionnaire.models import Constituency, Party
 
 class ConstituencyTests(TestCase):
     """ Test the Constituency model """
@@ -17,3 +17,22 @@ class ConstituencyTests(TestCase):
 
         constituency_again = Constituency.objects.get(id=constituency.id)
         self.assertEqual(name, constituency_again.name)
+
+class PartyTests(TestCase):
+    """ Tests for Party model """
+
+    def test_creation(self):
+        """ Create party, save, retrieve """
+
+        name = "Plaid Cymru"
+        abbreviation = "PC"
+
+        party = Party(name=name, abbreviation=abbreviation)
+        self.assertIsNone(party.id)
+        party.save()
+        self.assertIsNotNone(party.id)
+        self.assertIs(type(party.id), int)
+
+        party_again = Party.objects.get(id=party.id)
+        self.assertEqual(name, party_again.name)
+        self.assertEqual(abbreviation, party_again.abbreviation)
