ef13de515bb971252babb51ae641c301594bab06
Robin Winslow <robin@robinwinslow.co.uk>
2014-01-06 01:11:17 +0000

    Questionnaire form view

--- msq/urls.py
+++ msq/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from questionnaire.views import questionnaire_form, submit_form
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'msq.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', questionnaire_form, name="form"),
+    url(r'^submit/$', submit_form, name="submit"),
     url(r'^admin/', include(admin.site.urls)),
 )
--- questionnaire/views.py
+++ questionnaire/views.py
@@ -1,3 +1,57 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
+from questionnaire.models import Constituency, Party, AnswerSet
 
-# Create your views here.
+def questionnaire_form(request, error=''):
+    payload = {
+        'constituencies': Constituency.objects.all(),
+        'parties': Party.objects.all(),
+        'error': error,
+        'data': {
+            'constituency': '',
+            'party': '',
+            'voting': ''
+        }
+    }
+
+    if ('constituency' in request.POST and request.POST['constituency'].isdigit()):
+        payload['data']['constituency'] = int(request.POST['constituency'])
+
+    if ('party' in request.POST):
+        payload['data']['party'] = request.POST['party']
+
+    if ('going-to-vote' in request.POST):
+        payload['data']['voting'] = request.POST['going-to-vote']
+
+    return render(
+        request,
+        'questionnaire/form.html',
+        payload
+    )
+
+def submit_form(request):
+    if ('going-to-vote' not in request.POST):
+        return questionnaire_form(request, "Please say whether you're planning to vote")
+
+    going_to_vote = request.POST['going-to-vote']
+    constituency_id = request.POST['constituency']
+
+    if (not constituency_id.isdigit()):
+        return questionnaire_form(request, "Please select a constituency")
+
+    constituency = Constituency.objects.get(id=constituency_id)
+    parties = Party.objects.filter(abbreviation=request.POST['party'])
+    party = None
+
+    if (parties.count() > 0):
+        party = parties[0]
+
+    answer_set = AnswerSet(
+        constituency=constituency,
+        voting_for=party,
+        going_to_vote=request.POST['going-to-vote']
+    )
+    answer_set.save()
+
+    return HttpResponseRedirect(reverse('form'))
